d33ad1a7fdb61344f003ea50596546fc7791158d
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-10-08 15:24:12 +0100

    Playing with arrays unshift()

---
+++ playWithArrayMdn.js
@@ -0,0 +1,7 @@
+let myHobbiesList = ['running', 'reading', 'jigsaw', 'cycling', 'skiing'];
+
+myHobbiesList[2] = 'painting';
+myHobbiesList[4] = 'hiking';
+myHobbiesList.unshift('skiing');
+
+console.log(myHobbiesList);
