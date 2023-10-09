90597f3692fbd6fe60cd799c79d37d13fb6ffad5
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-10-09 11:36:41 +0100

    Sort array elements alphabetically sort()

---
+++ sortArrayElements.js
@@ -0,0 +1,9 @@
+/**
+ * Use the correct Array method to sort the fruits array alphabetically.
+ */
+
+ const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+
+ const fruitsSorted = fruits.sort();
+
+ console.log(fruitsSorted);
